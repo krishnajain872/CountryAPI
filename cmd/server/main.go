cadfa25bbf9c1e343dd0b74a0241200a99fa6e7b
anonymous <anonymous@hacker.com>
2026-01-11 11:27:12 +0530

    feat: entry point implementation

---
+++ cmd/server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Server is starting...")
+	// Server initialization logic goes here
+}	
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/krishnajain872/countery_search_api_cache_go_lang_assigememt
+
+go 1.25.0
